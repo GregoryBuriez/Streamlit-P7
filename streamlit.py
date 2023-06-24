bf1e251c304c66445e0b3b03646b4c44908166ce
Buriez Grégory <121756951+GregoryBuriez@users.noreply.github.com>
2023-06-23 21:17:24 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,6 +10,7 @@ import pickle
 from sklearn.neighbors import NearestNeighbors
 import random
 import requests
+import warnings
 import shap
 
 # Titre de l'application
@@ -18,14 +19,18 @@ st.title('Implémentez un modèle de scoring')
 # 1. Importation du dataframe et du modèle
 
 # Lecture du fichier CSV
-df = pd.read_csv("df_tabdashboard.csv", usecols=lambda col: col != 'TARGET', nrows=int(0.1 * pd.read_csv("df_tabdashboard.csv").shape[0]))  # Sélection de 10% des clients
+
+#df = pd.read_csv("df_tabdashboard.csv", usecols=lambda col: col != 'TARGET', nrows=int(0.1 * pd.read_csv("df_tabdashboard.csv").shape[0]))  # Sélection de 10% des clients
+df = pd.read_csv("df_tabdashboard.csv")
 liste_id = df['SK_ID_CURR'].tolist()
+
 
 with open('model_streamlit.pkl', 'rb') as file1:
     model1 = pickle.load(file1)
     
 with open('model_KNN_streamlit.pkl', 'rb') as file2:
     model2 = pickle.load(file2)
+
 
 st.subheader('Prédiction de notre modèle')
 
@@ -54,7 +59,6 @@ else:
     # Affichage du ratio du client en pourcentage avec une mise en forme personnalisée
     payment_rate = filtered_df["Taux d’endettement"].values[0] * 100
     st.write(f"<div style='display: flex; align-items: center; font-size: 15px;'>Endettement du client : <span style='font-size: 20px; font-weight: bold;'>{payment_rate}%</span></div>", unsafe_allow_html=True)
-
 
 # Appel de l'API :
 API_url = "https://buriez-flaskp7.herokuapp.com/api/predict"
@@ -91,56 +95,39 @@ if search_input:
             # Gérer les erreurs de requête
             st.write("Erreur lors de la requête à l'API")
 
-## Interprétabilité de la prédiction ##
-# En fonction de l'idenfiant présent dans le panel, nous allons connaître l'impact des variables #
+# Interprétabilité locale
 
-st.write('## Interprétabilité du résultat')                            
-shap.initjs()   
+# Créer un explainer SHAP avec le modèle
+explainer = shap.Explainer(model1, df)
 
-# Récupération des données d'entrée X
-if search_input and search_input.isdigit():
-    X = df[df['SK_ID_CURR'] == int(search_input)]
-else:
-    X = pd.DataFrame()  # Créez un DataFrame vide
+# Calculer les valeurs SHAP pour toutes les observations
+shap_values = explainer(df)
 
-# Vérification si les données d'entrée ne sont pas vides
-if not X.empty:
-    # Calcul des valeurs SHAP
-    explainer = shap.TreeExplainer(model1)
-    shap_values = explainer.shap_values(X)
-
-    # Affichage du texte expliquant la dépendance de la prédiction avec l'identifiant
-    st.write("L'interprétation de la prédiction peut varier en fonction de l'identifiant du client.")
-
-    # Appel de l'API pour chaque individu dans X
-    for i in range(len(X)):
-        client_id = X.iloc[i]['SK_ID_CURR']
-        response = requests.post(API_url, data={'client_id': client_id})
-
-        # Vérifier le statut de la réponse
-        if response.status_code == 200:
-            API_data = response.json()
-
-            # Vérifier si la prédiction existe dans les données renvoyées
-            if 'prediction' in API_data:
-                classe_predite = API_data['prediction']
-                if classe_predite == 1:
-                    etat = 'client à risque'
-                elif classe_predite == 0:
-                    etat = 'client peu risqué'
-                else:
-                    etat = 'Client non reconnu dans notre API'
-
-                # Afficher le résultat de l'interprétation pour chaque individu
-                st.markdown(f"<p>Interprétation pour le client avec l'identifiant {client_id} : {etat}</p>", unsafe_allow_html=True)
-
-    # Affichage du graphique SHAP
-    fig, ax = plt.subplots(figsize=(5, 5))
-    shap.summary_plot(shap_values, features=X, plot_type='bar', max_display=10, color_bar=False, plot_size=(10, 10))            
+# Fonction pour générer le graphique d'interprétation locale en fonction de l'identifiant
+def generate_local_interpretation_graph(identifier):
+    observation_index = df[df['SK_ID_CURR'] == identifier].index[0]
+    shap.initjs()  # Initialiser le support JavaScript
+    
+    # Créer une figure et un axe
+    fig, ax = plt.subplots()
+    shap.plots.waterfall(shap_values[observation_index, :], max_display=10, show=False)
+    
+    # Ajouter un ajustement de la mise en page
+    plt.tight_layout()
+    
+    # Afficher le graphique via st.pyplot()
     st.pyplot(fig)
-else:
-    st.write("Les données d'entrée sont vides.")
 
+# Vérifier si un identifiant valide a été spécifié par l'utilisateur
+if search_input != "" and search_input.isdigit():
+    # Convertir la valeur en entier
+    identifier = int(search_input)
+
+    # Générer le graphique d'interprétation locale
+    generate_local_interpretation_graph(identifier)
+else:
+    # Afficher un message d'erreur
+    st.write("Erreur : veuillez entrer un identifiant de client valide.")
 
 # Préparation données pour graphique
 
@@ -188,32 +175,27 @@ if not filtered_df.empty:
     # Affichage du graphique du diagramme circulaire (INCOME_CREDIT_PERC) à l'aide de Streamlit
     st.pyplot(fig_annuity)
 
-# Prédiction avec listing clients
 
-# Listing KNN
-# Récupérer les caractéristiques du client à prédire
 
-if search_input != "" and search_input.isdigit():
-    client_features = df[df['SK_ID_CURR'] == int(search_input)]
-
-    if client_features.empty:
-        st.write("Identifiant du client introuvable.")
-    else:
+# Prédiction avec le modèle KNN
+    if not filtered_df.empty:
+        
         # Prédiction des voisins les plus proches
-        prediction = model2.predict(client_features)
+        neighbors_indices = model2.kneighbors(df)[1][0]
 
         # Liste des identifiants des voisins les plus proches
-        nearest_neighbors_ids = [liste_id[i] for i in prediction.flatten()]
-
-        # Reste du code pour afficher les informations des voisins les plus proches...
+        nearest_neighbors_ids = [liste_id[i] for i in neighbors_indices]
 
         # Affichage des informations des voisins les plus proches
         # Sélection aléatoire de 2 clients parmi les voisins les plus proches
-        random_clients = random.sample(nearest_neighbors_ids, k=min(2, len(nearest_neighbors_ids)))
+        random_clients = random.sample(nearest_neighbors_ids, k=min(5, len(nearest_neighbors_ids)))
         random_clients_info = df[df['SK_ID_CURR'].isin(random_clients)][
             ['SK_ID_CURR', 'Sexe', 'Revenus annuels', 'Revenus totaux', 'Somme des crédits', "Taux d’endettement",
              'Propriétaire']]
 
         # Affichage des informations des clients sélectionnés de manière aléatoire
         st.write("Clients les plus proches :")
+        st.table(random_clients_info)
+else:
+    st.write("Veuillez spécifier un identifiant de client valide dans la barre de recherche.")
         st.table(random_clients_info)
